1dee4a5f9b449e9e399352d65aea6b5e8fb3c1f0
AmirTghizde <a.taghizadehmail@gmail.com>
2024-05-16 22:16:45 +0330

    innit commit

---
+++ src/main/java/com/taghizadeh/skillguru/SkillGuruApplication.java
@@ -0,0 +1,13 @@
+package com.taghizadeh.skillguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillGuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillGuruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taghizadeh/skillguru/SkillGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taghizadeh.skillguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillGuruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
